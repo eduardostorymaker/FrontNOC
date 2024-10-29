4278d63c08892541a0946763327ec02aff686c86
Eduardo Espinoza <C15380@TIM.COM.PE>
2024-10-28 23:24:43 -0500

    quitando ultimos mapas

--- app/test3/maps2/page.js
+++ app/test3/maps2/page.js
@@ -1,35 +1,36 @@
 "use client"
 
 import React from 'react';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-import MarkerClusterGroup from 'react-leaflet-markercluster';
+// import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+// import 'leaflet/dist/leaflet.css';
+// import 'leaflet.markercluster/dist/MarkerCluster.css';
+// import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+// import MarkerClusterGroup from 'react-leaflet-markercluster';
 
-const locations = [
-  { lat: 40.7128, lng: -74.0060 }, // New York
-  { lat: 40.7128, lng: -74.0060 }, // New York (duplicate for clustering)
-  { lat: 34.0522, lng: -118.2437 }, // Los Angeles
-  { lat: 41.8781, lng: -87.6298 }, // Chicago
-];
+// const locations = [
+//   { lat: 40.7128, lng: -74.0060 }, // New York
+//   { lat: 40.7128, lng: -74.0060 }, // New York (duplicate for clustering)
+//   { lat: 34.0522, lng: -118.2437 }, // Los Angeles
+//   { lat: 41.8781, lng: -87.6298 }, // Chicago
+// ];
 
 export default function MapWithClusters() {
   return (
-    <MapContainer
-      center={[39.8283, -98.5795]}
-      zoom={4}
-      style={{ height: '500px', width: '100%' }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <MarkerClusterGroup>
-        {locations.map((loc, i) => (
-          <Marker key={i} position={[loc.lat, loc.lng]} />
-        ))}
-      </MarkerClusterGroup>
-    </MapContainer>
+    // <MapContainer
+    //   center={[39.8283, -98.5795]}
+    //   zoom={4}
+    //   style={{ height: '500px', width: '100%' }}
+    // >
+    //   <TileLayer
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //   />
+    //   <MarkerClusterGroup>
+    //     {locations.map((loc, i) => (
+    //       <Marker key={i} position={[loc.lat, loc.lng]} />
+    //     ))}
+    //   </MarkerClusterGroup>
+    // </MapContainer>
+    <div></div>
   );
 }
